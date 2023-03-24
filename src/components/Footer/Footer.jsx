a8f7498074054c3bb0121a1cc2e27d190df903a7
hungnmZ <manhungg.99@gmail.com>
2023-03-24 10:37:00 +0700

    my solution

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,6 +10,7 @@ const Footer = () => {
         href="https://www.openpeeps.com/"
         target="_blank"
         rel="noopener noreferrer"
+        className={styles.footerDetail}
       >
         Open Peeps
       </a>
